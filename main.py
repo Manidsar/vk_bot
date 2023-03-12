2e1f156693838d9905932396d9b7e05a7d1064a7
Дмитрий Гуряков <99045619+Manidsar@users.noreply.github.com>
2023-03-12 04:44:50 +0000

    Оживил сервер

--- main.py
+++ main.py
@@ -1,61 +1,21 @@
 from flask import Flask, request, json
-# from pip._internal.vcs import git
-
 import messageHandler
-import vk_logic
-from multiprocessing import Process
-import settings
-from exts import db
-from flask_migrate import Migrate
-import git
+
+app = Flask(__name__)
 
 
-def register_extensions(app):
-    db.init_app(app)
+# def run():
+#   app.run(host='0.0.0.0', port=80)
+  
+# def keep_alive():
+#   t = Thread(target=run)
+#   t.start()
 
-
-def create_app():
-    app = Flask(__name__)
-    # app.config.from_object(config)
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-        username=settings.username,
-        password=settings.psw,
-        hostname=settings.hostname,
-        databasename=settings.databasename,
-    )
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-    app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-    app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    register_extensions(app)
-
-    return app
-
-
-app = create_app()
-migrate = Migrate(app, db)
-
+app.run(host='0.0.0.0', port=80)
 
 @app.route("/test", methods=["POST"])
 def nnd():
     return 'ok'
-
-
-# @app.route('/update_server', methods=['POST'])
-# def webhook():
-#     if request.method == 'POST':
-#         repo = git.Repo('/home/maniswear/mysite')
-#         origin = repo.remotes.origin
-#         repo.create_head('master',
-#         origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
-#         origin.pull()
-#         return 'Updated PythonAnywhere successfully', 200
-#     else:
-#         return 'Wrong event type', 400
 
 
 @app.route("/bots", methods=["POST"])
@@ -68,18 +28,12 @@ def main():
     if data['type'] == 'confirmation':
         return 'f99e17cd'
     elif data['type'] == "message_allow":
-        print('Мы обрабатываем наш запрос и что-то идет не так...')
-        user_id = data['object']['message']['from_id']
-        message_text = 'Это кнопка Начало'
-
-        vk_logic.send_some_msg(user_id, message_text)
-
         return 'ok'
     elif data['type'] == 'message_new':
-        # p = Process(target=messageHandler.create_answer, args=(data['object']['message'],))
+        # t = Thread(targer=messageHandler.create_answer, args=(data['object']['message'],))
         messageHandler.create_answer(data['object']['message'])
 
-        print('Сообщение о том, что обработка прошла успешно')
+        print('Обработка прошла успешно')
         return 'ok'
     elif data['type'] == 'message_reply':
         return 'ok'
@@ -89,3 +43,4 @@ def main():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return 'This is new login'
+
--- models.py
+++ models.py
@@ -1,36 +1,37 @@
 
-from exts import db
+from replit import db
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    surname = db.Column(db.String(100))
-    room = db.Column(db.Integer)
-    debt = db.Column(db.Integer)
-    admin = db.Column(db.Boolean(create_constraint=False))
-    register = db.Column(db.Boolean(create_constraint=False))
-    clown = db.Column(db.Boolean(create_constraint=False))
-    problem = db.Column(db.Boolean(create_constraint=False))
-    give_check = db.Column(db.Boolean(create_constraint=False))
-    status = db.Column(db.Boolean(create_constraint=False))
-    count = db.Column(db.Integer)
+# Тут будем объявлять нужные данные, чтобы названия столбцов были четкие.
+# class Student(db):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100))
+#     surname = db.Column(db.String(100))
+#     room = db.Column(db.Integer)
+#     debt = db.Column(db.Integer)
+#     admin = db.Column(db.Boolean(create_constraint=False))
+#     register = db.Column(db.Boolean(create_constraint=False))
+#     clown = db.Column(db.Boolean(create_constraint=False))
+#     problem = db.Column(db.Boolean(create_constraint=False))
+#     give_check = db.Column(db.Boolean(create_constraint=False))
+#     status = db.Column(db.Boolean(create_constraint=False))
+#     count = db.Column(db.Integer)
 
-    def __repr__(self):
-        # данные, которые выдает модель при обращении к объекту
-        # return {
-        #         'id': self.id,
-        #         'name': self.name,
-        #         'surname': self.surname,
-        #         'room': self.room,
-        #         'dept': self.dept,
-        #         'admin': self.admin,
-        #         'register': self.register,
-        #         'clown': self.clown,
-        #         'problem': self.problem,
-        #         'give_check': self.give_check,
-        #         'status': self.status,
-        #         }
-        return f'<Student {self.name}>'
+    # def __repr__(self):
+    #     # данные, которые выдает модель при обращении к объекту
+    #     # return {
+    #     #         'id': self.id,
+    #     #         'name': self.name,
+    #     #         'surname': self.surname,
+    #     #         'room': self.room,
+    #     #         'dept': self.dept,
+    #     #         'admin': self.admin,
+    #     #         'register': self.register,
+    #     #         'clown': self.clown,
+    #     #         'problem': self.problem,
+    #     #         'give_check': self.give_check,
+    #     #         'status': self.status,
+    #     #         }
+    #     return f'<Student {self.name}>'
 
 
 def get_status(id_s):
@@ -46,6 +47,12 @@ def get_status(id_s):
 
     # Функция, которая изменяет имя и фамилию или меняет их принимает id и сообщение, которое нужно обработать.
     # Возвращает соообщение об успешной регистрации или ошибке
+
+def start_register(id):
+  db[id] = {
+    'name': False
+  }
+  
 def registration(id, message):
     base = get_status(id)
     try:
